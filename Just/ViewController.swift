9bc6b8b79140d9c52bf8f4fa6c2b9fc3c5f9a069
Alex Goncharov <x401om@gmail.com>
2017-01-29 13:30:21 +0300

    Web view

--- Just/ViewController.swift
+++ Just/ViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
-
+// CHntIUEWQvtPQK7MTOMX8SdMAZtsFcM17HgUxsFQ
 class ViewController: UIViewController {
 
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var webView: UIWebView!
+
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  @IBAction func loadButtonPressed(sender: UIButton) {
+    
   }
-
-
 }
 
